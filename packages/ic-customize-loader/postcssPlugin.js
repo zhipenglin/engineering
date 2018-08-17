db11f32a277f5e1208b06ea25ece204b5f840a6b
linzhipeng <zhipeng.lin@ifchange.com>
2018-08-17 10:21:14 +0800

    支持不写url的语法

--- packages/ic-customize-loader/postcssPlugin.js
+++ packages/ic-customize-loader/postcssPlugin.js
@@ -14,36 +14,56 @@ const resolvePath = (targetPath, rules, basePath) => {
     }
 };
 
-const transform = (value, rules, basePath) => {
+const addSuffix = (value, extname, suffixList) => {
+    if (suffixList.indexOf(path.extname(value)) === -1) {
+        value += extname;
+    }
+    return value;
+};
+
+const transform = (value, rules, basePath, extname, suffixList) => {
     const urlParser = valueParser(value);
+    const parse=(value)=>{
+        rulesMatch(rules, value, (test) => {
+            const targetPath = resolvePath(addSuffix(value, extname, suffixList), rules, basePath);
+            if (targetPath) {
+                value = targetPath;
+            } else {
+                value = value.replace(test, '');
+            }
+        });
+        return value;
+    }
     urlParser.walk((node) => {
         if (node.type === 'function' && node.value === 'url') {
             node.nodes.forEach((target) => {
                 if (target.type === 'string') {
-                    rulesMatch(rules, target.value, (test) => {
-                        const targetPath = resolvePath(target.value, rules, basePath);
-                        if (targetPath) {
-                            target.value = targetPath;
-                        } else {
-                            target.value = target.value.replace(test, '');
-                        }
-                    });
+                    target.value=parse(target.value);
                 }
             });
+        }else if(node.type==='string'){
+            node.value=parse(node.value);
         }
     });
     return urlParser.toString();
 };
 
 module.exports = postcss.plugin('customize', options => {
+    options = Object.assign({
+        rules: [],
+        suffix: []
+    }, options);
     return (css) => {
-        const basePath = path.dirname(css.source.input.file);
+        const basePath = path.dirname(css.source.input.file),
+            extname = path.extname(css.source.input.file),
+            suffixList = ['.css', '.scss', '.sass', '.less', ...options.suffix];
+
         css.walk(rule => {
-            if(rule.params){
-                rule.params = transform(rule.params, options.rules, basePath);
+            if (rule.params) {
+                rule.params = transform(rule.params, options.rules, basePath, extname, suffixList);
             }
-            rule.walkDecls&&rule.walkDecls(decl => {
-                decl.value = transform(decl.value, options.rules, basePath);
+            rule.walkDecls && rule.walkDecls(decl => {
+                decl.value = transform(decl.value, options.rules, basePath, extname, suffixList);
             });
         });
     }
